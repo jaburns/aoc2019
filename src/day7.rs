5760d2cda621d7fd018f48f1c5eb5c6ec8daf40c
Jeremy Burns <j@jaburns.net>
2019-12-07 15:12:52 -0700

    Finished day 7

--- src/day7.rs
+++ src/day7.rs
@@ -1,7 +1,7 @@
-use crate::vm::IntCodeMachine;
+use crate::vm::{IntCodeMachine,RunResult};
 use permutohedron::Heap;
 
-fn run_amplifier_circuit(tape: &Vec<i32>, phase_seq: &[i32; 5]) -> i32 {
+fn run_forward_amplifier_circuit(tape: &Vec<i32>, phase_seq: &[i32; 5]) -> i32 {
     let a = IntCodeMachine::run_all(&tape, &[phase_seq[0], 0]).pop().unwrap();
     let b = IntCodeMachine::run_all(&tape, &[phase_seq[1], a]).pop().unwrap();
     let c = IntCodeMachine::run_all(&tape, &[phase_seq[2], b]).pop().unwrap();
@@ -10,12 +10,50 @@ fn run_amplifier_circuit(tape: &Vec<i32>, phase_seq: &[i32; 5]) -> i32 {
     e
 }
 
-fn find_best_thruster_signal(tape: &Vec<i32>) -> i32 {
-    let mut phases = [0i32,1,2,3,4];
-    let mut heap = Heap::new(&mut phases);
+fn run_feedback_amplifier_circuit(tape: &Vec<i32>, phase_seq: &[i32; 5]) -> i32 {
+    let mut signal = 0i32;
+    let mut machine_index = 0usize;
+    let mut machines = [
+        IntCodeMachine::new(tape),
+        IntCodeMachine::new(tape),
+        IntCodeMachine::new(tape),
+        IntCodeMachine::new(tape),
+        IntCodeMachine::new(tape)
+    ];
+
+    for i in 0..machines.len() {
+        machines[i].run();
+        machines[i].provide_input(phase_seq[i]);
+    }
+
+    loop {
+        match machines[machine_index].run() {
+            RunResult::RequiresInput => machines[machine_index].provide_input(signal),
+            RunResult::Halted => break,
+            RunResult::ProvidingOutput(_) => panic!("Expected input or halt")
+        }
+
+        match machines[machine_index].run() {
+            RunResult::RequiresInput => panic!("Expected output or halt"),
+            RunResult::Halted => break,
+            RunResult::ProvidingOutput(x) => {
+                signal = x;
+                machine_index = (machine_index + 1) % 5
+            }
+        }
+    }
+
+    signal
+}
+
+fn find_best_thruster_signal(tape: &Vec<i32>, phases: &[i32;5], run: fn(&Vec<i32>, &[i32;5]) -> i32) -> i32 {
+    let mut mut_phases = [0i32; 5];
+    mut_phases.copy_from_slice(phases);
+
+    let mut heap = Heap::new(&mut mut_phases);
     let mut best_signal = 0i32;
     while let Some(perm) = heap.next_permutation() {
-        let new_signal = run_amplifier_circuit(tape, perm);
+        let new_signal = run(tape, perm);
         if new_signal > best_signal { best_signal = new_signal };
     }
     best_signal
@@ -27,8 +65,8 @@ pub fn main() {
         .map(|x| x.trim().parse().unwrap())
         .collect();
 
-    let result0 = find_best_thruster_signal(&tape);
-    let result1 = 0i32;
+    let result0 = find_best_thruster_signal(&tape, &[0,1,2,3,4], run_forward_amplifier_circuit);
+    let result1 = find_best_thruster_signal(&tape, &[5,6,7,8,9], run_feedback_amplifier_circuit);
 
     println!("{} {}", result0, result1);
 }
