4a0f9b7698e5cf87dab108fe3930f9b8fe45e030
Jeremy Burns <j@jaburns.net>
2019-12-06 21:52:44 -0700

    Completed day 5 part 2

--- src/day5.rs
+++ src/day5.rs
@@ -1,15 +1,20 @@
-const IMI_HALT:   i32 = 99;
-const IMI_ADD:    i32 =  1;
-const IMI_MUL:    i32 =  2;
-const IMI_INPUT:  i32 =  3;
-const IMI_OUTPUT: i32 =  4;
+const IMI_HALT: i32 = 99;
+const IMI_ADD:  i32 = 01;
+const IMI_MUL:  i32 = 02;
+const IMI_IN:   i32 = 03;
+const IMI_OUT:  i32 = 04;
+const IMI_JNZ:  i32 = 05;
+const IMI_JZ:   i32 = 06;
+const IMI_LESS: i32 = 07;
+const IMI_EQ:   i32 = 08;
 
-fn arg_is_immediate(instruction: i32, arg: i32) -> bool {
-    let mut ret = instruction / 100;
+fn get_arg(tape: &[i32], ip: usize, arg: usize) -> i32 {
+    let mut arg_digit = tape[ip] / 100;
     for _ in 0..arg {
-        ret /= 10;
+        arg_digit /= 10;
     }
-    ret % 10 != 0
+
+    if arg_digit % 10 != 0 { tape[ip+arg+1] } else { tape[tape[ip+arg+1] as usize] }
 }
 
 fn execute_tape(tape: &[i32], inputs: &[i32]) -> Vec<i32> {
@@ -23,33 +28,69 @@ fn execute_tape(tape: &[i32], inputs: &[i32]) -> Vec<i32> {
             IMI_HALT => break,
 
             IMI_ADD => { 
-                let arg0 = if arg_is_immediate(p[i], 0) { p[i+1] } else { p[p[i+1] as usize] };
-                let arg1 = if arg_is_immediate(p[i], 1) { p[i+2] } else { p[p[i+2] as usize] };
-                let addr_out = p[i+3];
-                p[addr_out as usize] = arg0 + arg1;
+                let arg0 = get_arg(&p, i, 0);
+                let arg1 = get_arg(&p, i, 1);
+                let arg2 = p[i + 3];
+                p[arg2 as usize] = arg0 + arg1;
                 i += 4;
             },
 
             IMI_MUL => {
-                let arg0 = if arg_is_immediate(p[i], 0) { p[i+1] } else { p[p[i+1] as usize] };
-                let arg1 = if arg_is_immediate(p[i], 1) { p[i+2] } else { p[p[i+2] as usize] };
-                let addr_out = p[i+3];
-                p[addr_out as usize] = arg0 * arg1;
+                let arg0 = get_arg(&p, i, 0);
+                let arg1 = get_arg(&p, i, 1);
+                let arg2 = p[i + 3];
+                p[arg2 as usize] = arg0 * arg1;
                 i += 4;
             },
 
-            IMI_INPUT => {
+            IMI_IN => {
                 let input = inputs[input_ptr];
                 input_ptr += 1;
-                let addr_out = p[i+1];
-                p[addr_out as usize] = input;
+                let arg0 = p[i + 1];
+                p[arg0 as usize] = input;
                 i += 2;
             },
 
-            IMI_OUTPUT => {
-                let arg0 = if arg_is_immediate(p[i], 0) { p[i+1] } else { p[p[i+1] as usize] };
+            IMI_OUT => {
+                let arg0 = get_arg(&p, i, 0);
                 outputs.push(arg0);
                 i += 2;
+            },
+
+            IMI_JNZ => {
+                let arg0 = get_arg(&p, i, 0);
+                let arg1 = get_arg(&p, i, 1);
+                if arg0 != 0 {
+                    i = arg1 as usize;
+                } else {
+                    i += 3;
+                }
+            },
+
+            IMI_JZ => {
+                let arg0 = get_arg(&p, i, 0);
+                let arg1 = get_arg(&p, i, 1);
+                if arg0 == 0 {
+                    i = arg1 as usize;
+                } else {
+                    i += 3;
+                }
+            },
+
+            IMI_LESS => {
+                let arg0 = get_arg(&p, i, 0);
+                let arg1 = get_arg(&p, i, 1);
+                let arg2 = p[i + 3];
+                p[arg2 as usize] = if arg0 < arg1 { 1 } else { 0 };
+                i += 4;
+            },
+
+            IMI_EQ => {
+                let arg0 = get_arg(&p, i, 0);
+                let arg1 = get_arg(&p, i, 1);
+                let arg2 = p[i + 3];
+                p[arg2 as usize] = if arg0 == arg1 { 1 } else { 0 };
+                i += 4;
             },
 
             _ => panic!("ABORTED: Encountered unknown opcode {} at location {}", p[i], i)
@@ -65,9 +106,11 @@ pub fn main() {
         .map(|x| x.trim().parse().unwrap())
         .collect();
 
-    let result0 = execute_tape(&tape, &[1]);
+    let results0 = execute_tape(&tape, &[1]);
+    let results1 = execute_tape(&tape, &[5]);
 
-    for i in 0..result0.len() {
-        println!("{}", result0[i]);
-    }
+    let result0 = results0[results0.len()-1];
+    let result1 = results1[results1.len()-1];
+
+    println!("{} {}", result0, result1);
 }
