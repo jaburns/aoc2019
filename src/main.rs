c169f17139358010c7ed3adf7efaed4e7ec645a3
Jeremy Burns <j@jaburns.net>
2019-12-13 15:39:45 -0700

    Finished day 12

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod day8;
 mod day9;
 mod day10;
 mod day11;
+mod day12;
 mod intcode;
 
 fn run_from_arg(arg: i32) {
@@ -24,12 +25,13 @@ fn run_from_arg(arg: i32) {
         9 => day9::main(),
         10 => day10::main(),
         11 => day11::main(),
+        12 => day12::main(),
         _ => {}
     }
 }
 
 fn run_default() {
-    day11::main()
+    day12::main()
 }
 
 fn main() {
